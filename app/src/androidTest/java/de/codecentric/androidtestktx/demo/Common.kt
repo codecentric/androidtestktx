067a6df771d5f57515a3e64ff91bc47c941ca9e5
Dusko Bajic <bajicdusko@gmail.com>
2018-11-07 12:36:07 +0100

    #10 PR fix.

--- app/src/androidTest/java/de/codecentric/androidtestktx/demo/Common.kt
+++ app/src/androidTest/java/de/codecentric/androidtestktx/demo/Common.kt
@@ -3,11 +3,6 @@ package de.codecentric.androidtestktx.demo
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
-/**
- * Created by Dusko Bajic
- * GitHub @bajicdusko
- */
-
 const val INPUT_TEXT = "Type text"
 const val REPLACE_TEXT = "Replace text"
 
